8246c7976eaa7afc29868f05bf6a4d60440645df
Ernesto98523 <65617486+Ernesto98523@users.noreply.github.com>
2020-06-08 12:39:22 -0500

    Create FuncionPartePrincipal.js

---
+++ FuncionPartePrincipal.js
@@ -0,0 +1,24 @@
+var serv1 =document.getElementById("producto1");
+var serv2=document.getElementById("producto2");
+var serv3=document.getElementById("producto3");
+var serv4=document.getElementById("producto4");
+var  kars= document.getElementById("btncalcular");
+var totiP=document.getElementById("btnpagar");
+
+kars.addEventListener("onclick" funcionCalcular);
+function funcionCalcular() {
+    serv1="";
+    serv2="";
+    serv3="";
+    serv4="";
+    switch (funcionCalcular) {
+        case expression:if (serv1.checked) {
+            (serv1*350;)
+            return "Tu resultado es este" + serv1;
+        }
+
+            break;
+        default:
+
+    }
+}
